37231958693feb97e913885ff16db4dee671f732
j2ieu <j2ieu@pm.me>
2021-06-21 20:15:31 +0900

    s2/week7 Binary Search

--- week2/gbeea1004/circulardoubly/main/CircularDoubleLinkedListException.java
+++
@@ -1,9 +0,0 @@
-public class CircularDoubleLinkedListException extends RuntimeException {
-    public CircularDoubleLinkedListException() {
-        super();
-    }
-
-    public CircularDoubleLinkedListException(String message) {
-        super(message);
-    }
-}
